848f9649c2ee11cac6a4174217046ad1b2b21fa9
Yuvraj Dhakate <ydhakate@calibo.com>
2024-08-21 11:04:29 +0530

    update testing lib

--- src/test/listing-page.test.js
+++ src/test/listing-page.test.js
@@ -1,15 +1,12 @@
 import React from "react";
-import Enzyme from "enzyme";
-import { shallow } from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-import "jest-enzyme";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Listing from '../listing';
 
 jest.mock("highcharts/highcharts-3d");
 
-Enzyme.configure({ adapter: new Adapter() });
 describe("<Listing />", () => {
   it("renders <Listing /> component in root", () => {
-    shallow(<Listing />);
+    render(<Listing />);
   });
 });
--- src/test/page-not-found.test.js
+++ src/test/page-not-found.test.js
@@ -1,15 +1,12 @@
 import React from "react";
-import Enzyme from "enzyme";
-import { shallow } from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-import "jest-enzyme";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import PageNotFound from '../page-not-found';
 
 jest.mock("highcharts/highcharts-3d");
 
-Enzyme.configure({ adapter: new Adapter() });
 describe("<PageNotFound />", () => {
   it("renders <PageNotFound /> component in root", () => {
-    shallow(<PageNotFound />);
+    render(<PageNotFound />);
   });
 });
